2ffc8f19ebed1b0c1cb19ef2c94dc2d84f253184
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-03 21:28:31 +0100

    Fixed bug, wasn't signing the transfer instruction with the escrow_account  PDA

--- programs/anchor-escrow/src/lib.rs
+++ programs/anchor-escrow/src/lib.rs
@@ -69,17 +69,18 @@ pub mod anchor_escrow {
         let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
 
         token::transfer(
-            ctx.accounts.into_transfer_to_initializer_context(),
+        ctx.accounts.into_transfer_to_initializer_context(),
             ctx.accounts.escrow_account.taker_amount,
         )?;
 
         token::transfer(
-            ctx.accounts.into_transfer_to_taker_context(),
+        ctx.accounts.into_transfer_to_taker_context()
+                        .with_signer(&[&authority_seeds[..]]),
             ctx.accounts.escrow_account.initializer_amount,
         )?;
 
         token::close_account(
-            ctx.accounts.into_close_context()
+        ctx.accounts.into_close_context()
                         .with_signer(&[&authority_seeds[..]]),
         )?;
 
