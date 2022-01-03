e9c22abf655d5ed6c47e0d23a41ac64c31134a77
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-03 20:24:52 +0100

    Created implementation for Initialize to convert the Initialize Context into the appropriate CpiContext

--- programs/anchor-escrow/src/lib.rs
+++ programs/anchor-escrow/src/lib.rs
@@ -133,3 +133,26 @@ pub struct EscrowAccount {
     pub taker_amount:  u64,
 }
 
+impl<'info> Initialize<'info> {
+    fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+
+        let cpi_accounts = Transfer {
+            from: self.initializer_deposit_token_account.to_account_info().clone(),
+            to: self.vault_account.to_account_info().clone(),
+            authority: self.initializer.clone(),
+        };
+
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        
+        let cpi_accounts = SetAuthority {
+            account_or_mint: self.vault_account.to_account_info().clone(),
+            current_authority: self.initializer.clone(),
+        };
+
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+}
+
