7635e1cc87eaf8c93dffe74e214d9b4e6b556f45
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-03 19:26:13 +0100

    Created scaffold for the three processor Instruction functions our program can receive.

--- programs/anchor-escrow/src/lib.rs
+++ programs/anchor-escrow/src/lib.rs
@@ -5,7 +5,23 @@ declare_id!("CGcc7NHAsJ1zdbx47F8vcQ4qh8Z5JzXnsbNgSjcQh2kp");
 #[program]
 pub mod anchor_escrow {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        _vault_account_bump: u8,
+        initializer_amount: u64,
+        taker_amount: u64,
+    ) -> ProgramResult {
+        // TODO
+        Ok(())
+    }
+
+    pub fn cancel(ctx: Context<Cancel>) -> ProgramResult {
+        // TODO
+        Ok(())
+    }
+
+    pub fn exchange(ctx: Context<Exchange>) -> ProgramResult {
+        // TODO
         Ok(())
     }
 }
